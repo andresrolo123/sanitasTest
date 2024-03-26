24da69f99b082be955dfc81b2dcb7db14ffcb692
andresrolo123 <andressdlm@gmail.com>
2024-03-26 12:42:51 +0100

    Adding base dependecies, POC tracerAPI

---
+++ src/main/java/org/calculadora/Calculadora.java
@@ -0,0 +1,12 @@
+package org.calculadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calculadora {
+
+    public static void main(String[] args)  {
+        SpringApplication.run(Calculadora.class, args);
+    }
+}
